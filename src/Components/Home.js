aa831cb2251f99191de56c610aa7772abd6655f4
Flynn Burklin <fburklin@us.ci.org>
2020-09-29 12:02:18 -0600

    Encourage button now confetti style, photo ring now has entropy, two rings, animation

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,31 +1,43 @@
 import React from 'react'
 import img1 from '../photos/passion-project-img1.jpg'
+import ConfettiButton from './ConfettiButton'
 
 const Home = () => {
 
-  const imageCount = 6
+  const rings = [
+    { count: 6, baseRadius: 300 },
+    { count: 15, baseRadius: 600 }
+  ]
+  const imageCount = 21
   const renderImages = (imageCount) => {
-    let images = []
+    const images = []
     const centerY = window.innerHeight / 2
     const centerX = window.innerWidth / 2
-    const imgClasses = ['imgLarge', 'imgMedium', 'imgSmall']
-    for (let i = 0; i < imageCount; i++) {
-      const positionStyle = {
-        left: `${centerX + Math.cos(Math.PI * i / 3) * 350}px`,
-        top: `${centerY + Math.sin(Math.PI * i / 3) * 350}px`
+    const imgClasses = [
+      { className: 'imgLarge', size: 150 },
+      { className: 'imgMedium', size: 125 },
+      { className: 'imgSmall', size: 100 }]
+
+    for (const currentRing of rings) {
+      for (let i = 0; i < currentRing.count && images.length < imageCount; i++) {
+        const imageClass = imgClasses[Math.round(Math.random() * 2)]
+        const angleEntropy = (Math.random() * Math.PI / 9) - (Math.PI / 18)
+        const radiusEntropy = Math.round(Math.random() * currentRing.baseRadius / 6)
+        const positionStyle = {
+          left: `${centerX - imageClass.size + Math.cos((2 * Math.PI * i / currentRing.count) + angleEntropy) * (currentRing.baseRadius + radiusEntropy)}px`,
+          top: `${centerY - imageClass.size + Math.sin((2 * Math.PI * i / currentRing.count) + angleEntropy) * (currentRing.baseRadius + radiusEntropy)}px`
+        }
+        images.push(
+          <img className={`imgBase ${imageClass.className}`} src={img1} style={positionStyle} />
+        )
       }
-      images.push(
-        <img className={imgClasses[Math.round(Math.random() * 2)]} src={img1} style={positionStyle} />
-      )
     }
     return images
   }
 
   return (
     <div className='Home'>
-      <div className='EncouragementCTA'>
-        ENCOURAGE A CHILD OR ELSE...
-      </div>
+      <ConfettiButton />
       {renderImages(imageCount)}
     </div>
   )
