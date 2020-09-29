707c9781e5183c9ae8607c94b2d6bd0664d982c6
Matthew <murbina@us.ci.org>
2020-09-29 16:48:17 -0400

    add random bio and textarea and button to encourage

--- src/Components/Encourage.js
+++ src/Components/Encourage.js
@@ -1,20 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import childInfo from '../utils/children/index'
 
 const Encourage = () => {
+  const [randomChild, setRandomChild] = useState(undefined)
+
+  useEffect(() => {
+    const randomChild = async () => {
+      const random = await childInfo.getRandomChild()
+      setRandomChild(random)
+    }
+    randomChild()
+  }, [])
+
+  let childName = null
+  let childBio = null
+  if (randomChild) {
+    childName = randomChild.name
+    childBio = (
+      <div className='Bio-Info'>
+        <img src={randomChild.image} />
+        <div>
+          Name: {randomChild.name}
+        </div>
+        <div>
+          Age: {randomChild.age}
+        </div>
+        <div>
+          Gender: {randomChild.gender}
+        </div>
+        <div>
+          Country: {randomChild.country}
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div className='Encourage-View'>
+    <div className='Encouragement-View'>
       <div className='Child-Bio'>
-        child data
-        child data
-        child data
-        child data
-        child data
-        child data
-        child data
+        {childBio}
       </div>
       <div className='Encouragement'>
-        Write an encouraging message to
+        Write an encouraging message to {childName}
         <textarea className='Encouragement-Text' />
+        <div className='EncouragementCTA'>
+          Send Encouragement
+        </div>
       </div>
     </div>
   )
